2352d24d3bd4e06359389ede4b7d4bae0c2da972
colemanja91 <colemanja91@gmail.com>
2020-03-25 20:10:31 -0400

    Start a new round
    https://trello.com/c/qUFgnXMH/56-graphql-mutation-start-a-new-round

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -3,6 +3,7 @@ module Types
     field :add_video_link, Types::GameType, mutation: Mutations::AddVideoLink
     field :join_game, Types::GameType, mutation: Mutations::JoinGame
     field :new_game, Types::GameType, mutation: Mutations::NewGame
+    field :new_round, Types::GameType, mutation: Mutations::NewRound
     field :play_cards, Types::GameType, mutation: Mutations::PlayCards
     field :pick_winner, Types::GameType, mutation: Mutations::PickWinner
     field :start_game, Types::GameType, mutation: Mutations::StartGame
---
+++ spec/graphql/mutations/new_round_spec.rb
@@ -0,0 +1,54 @@
+require 'rails_helper'
+
+RSpec.describe Mutations::NewRound do
+  let(:game) { create(:game, as_status: "started") }
+
+  let(:mutation) {
+    <<~GQL
+      mutation {
+        newRound(gameId: #{game.id}) {
+          id
+          currentRound {
+            user {
+              id
+            }
+            blackCard {
+              text
+              pick
+            }
+            round
+            status
+          }
+        }
+      }
+    GQL
+  }
+
+  before do
+    game.user_games.each do |user_game|
+      pick = game.current_round.black_card.pick
+      card_ids = user_game.hand.sample(pick).pluck(:id)
+      user_game.play_cards(card_ids)
+    end
+  end
+
+  it "ends the existing round" do
+    first_round = game.current_round
+    expect(first_round.status).to eq("submitted")
+    IsolationgamesSchema.execute(mutation, context: { current_user: game.user })
+    expect(first_round.reload.status).to eq("ended")
+  end
+
+  it "starts a new round" do
+    first_round = game.current_round
+    IsolationgamesSchema.execute(mutation, context: { current_user: game.user })
+    expect(game.reload.current_round).not_to eq(first_round)
+    expect(game.reload.current_round.status).to eq("started")
+  end
+
+  it "returns the new round" do
+    result = IsolationgamesSchema.execute(mutation, context: { current_user: game.user })["data"]["newRound"]
+    expect(result["currentRound"]["round"]).to eq(game.reload.current_round.round)
+    expect(result["currentRound"]["blackCard"]["text"]).to eq(game.reload.current_round.black_card.text)
+  end
+end
