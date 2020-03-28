23bbd2aa30cc0be8412323b15b247044ce95ae0f
colemanja91 <colemanja91@gmail.com>
2020-03-27 22:00:07 -0400

    add specs

--- spec/graphql/mutations/join_game_spec.rb
+++ spec/graphql/mutations/join_game_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe Mutations::JoinGame do
@@ -19,41 +21,41 @@ RSpec.describe Mutations::JoinGame do
   end
 
   it 'returns the game' do
-    result = mutation(game.name)["data"][mutation_name]
-    expect(Game.find(result["id"]).name).to eq(result["name"])
+    result = mutation(game.name)['data'][mutation_name]
+    expect(Game.find(result['id']).name).to eq(result['name'])
   end
 
-  it "joins the user to the game" do
-    expect { mutation(game.name) }.to(change{ user.user_games.count }.by 1)
+  it 'joins the user to the game' do
+    expect { mutation(game.name) }.to(change { user.user_games.count }.by(1))
   end
 
-  it "returns the game if the user has already joined" do
-    user.join_game!(game)
-    result = mutation(game.name)["data"][mutation_name]
-    expect(Game.find(result["id"]).name).to eq(result["name"])
-  end
-
-  it "does not create another user_game if they user has already joined" do
+  it 'does not create another user_game if they user has already joined' do
     user.join_game!(game)
     expect { mutation(game.name) }.not_to(change { user.user_games.count })
   end
 
-  it "rejoins the user if they have already left" do
+  it 'rejoins the user if they have already left' do
     user_game = user.join_game!(game)
     user_game.leave!
-    result = mutation(game.name)["data"][mutation_name]
-    expect(Game.find(result["id"]).name).to eq(result["name"])
+    result = mutation(game.name)['data'][mutation_name]
+    expect(Game.find(result['id']).name).to eq(result['name'])
   end
 
-  it "raises an error if no game found" do
-    result = mutation("thisisnotaname")
-    expect(result["errors"]).not_to be_nil
+  it 'raises an error if the user is already in another game' do
+    create(:game, user: user, as_status: 'started')
+    result = mutation(game.name)
+    expect(result['errors']).not_to be_nil
   end
 
-  it "raises an error if the game has ended" do
+  it 'raises an error if no game found' do
+    result = mutation('thisisnotaname')
+    expect(result['errors']).not_to be_nil
+  end
+
+  it 'raises an error if the game has ended' do
     game.update!(status: :started)
     game.update!(status: :ended)
     result = mutation(game.name)
-    expect(result["errors"]).not_to be_nil
+    expect(result['errors']).not_to be_nil
   end
 end
