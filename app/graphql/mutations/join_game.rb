2af4b905e792facf12b98088345112e640fb58f9
colemanja91 <colemanja91@gmail.com>
2020-03-27 21:51:14 -0400

    add validation and error message to GraphQL

--- app/graphql/mutations/join_game.rb
+++ app/graphql/mutations/join_game.rb
@@ -9,6 +9,11 @@ class Mutations::JoinGame < Types::BaseMutation
 
   def resolve(game_name:)
     current_user = context[:current_user]
+
+    if current_user.games.active.present? || current_user.user_games.joined.present?
+      raise GraphQL::ExecutionError, 'User has already joined a game.'
+    end
+
     game = Game.active.find_by(name: game_name)
     raise GraphQL::ExecutionError, 'Game not found' unless game
 
--- app/graphql/mutations/new_game.rb
+++ app/graphql/mutations/new_game.rb
@@ -1,10 +1,17 @@
+# frozen_string_literal: true
+
 class Mutations::NewGame < Types::BaseMutation
   null false
 
-  description "Start a new game"
+  description 'Start a new game'
 
-  def resolve()
+  def resolve
     current_user = context[:current_user]
+
+    if current_user.games.active.present? || current_user.user_games.joined.present?
+      raise GraphQL::ExecutionError, 'User has already joined a game.'
+    end
+
     game = current_user.games.create!
     current_user.join_game!(game)
 
