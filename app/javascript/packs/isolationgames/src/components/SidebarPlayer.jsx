0598319c72d702443920a93ed733a21ac33c1dec
colemanja91 <colemanja91@gmail.com>
2020-03-29 13:41:23 -0400

    Show chip for judge
    https://trello.com/c/fAREXdrm/70-when-round-has-started-display-judge-chip

--- app/javascript/packs/isolationgames/src/components/SidebarPlayer.jsx
+++ app/javascript/packs/isolationgames/src/components/SidebarPlayer.jsx
@@ -2,25 +2,28 @@ import React, { Fragment } from "react";
 import ListItem from "@material-ui/core/ListItem";
 import Typography from "@material-ui/core/Typography";
 import Chip from "@material-ui/core/Chip";
-//import "../../assets/stylesheets/components/SidebarPlayer.scss";
 
-function SidebarPlayer({ player, index }) {
+function SidebarPlayer({ player }) {
+  const chips = () => {
+    return (
+      <Fragment>
+        {player.isOwner ? <Chip label="Game Owner" color="primary" /> : null}
+        {player.isJudge ? <Chip label="Judge" color="secondary" /> : null}
+        {player.status == "left" ? <Chip label="left" /> : null}
+      </Fragment>
+    );
+  };
+
+  const playerColor = () => {
+    return player.status == "left" ? "error" : "initial";
+  };
+
   return (
     <ListItem className="sidebar-item">
-      {player.isOwner ? (
-        <Fragment>
-          <Typography>{player.userDisplayName}</Typography>
-          <Chip label="Game Owner" />
-        </Fragment>
-      ) : (
-        <Typography>
-          {player.status == "left" ? (
-            <i>{player.userDisplayName} (left)</i>
-          ) : (
-            player.userDisplayName
-          )}
-        </Typography>
-      )}
+      <Fragment>
+        <Typography color={playerColor()}>{player.userDisplayName}</Typography>
+        {chips()}
+      </Fragment>
     </ListItem>
   );
 }
