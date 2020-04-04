c2ae96438141dfc87e7e699afb16baf1f8f64966
colemanja91 <colemanja91@gmail.com>
2020-04-04 10:39:11 -0400

    Show how many rounds each user has won
    https://trello.com/c/vB64hD3X/81-show-number-of-won-rounds-as-a-chip

--- app/javascript/packs/isolationgames/src/components/SidebarPlayer.jsx
+++ app/javascript/packs/isolationgames/src/components/SidebarPlayer.jsx
@@ -4,12 +4,21 @@ import Typography from "@material-ui/core/Typography";
 import Chip from "@material-ui/core/Chip";
 
 function SidebarPlayer({ player }) {
+  const wonRoundsChipText = () => {
+    return player.wonRounds > 1
+      ? `${player.wonRounds} wins`
+      : `${player.wonRounds} win`;
+  };
+
   const chips = () => {
     return (
       <div>
         {player.isOwner ? <Chip label="Game Owner" color="primary" /> : null}
         {player.isJudge ? <Chip label="Judge" color="secondary" /> : null}
         {player.hasPlayed ? <Chip label="Has played" color="default" /> : null}
+        {player.wonRounds > 0 ? (
+          <Chip label={wonRoundsChipText()} color="secondary" />
+        ) : null}
         {player.status == "left" ? <Chip label="left" /> : null}
       </div>
     );
