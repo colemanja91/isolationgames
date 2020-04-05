76c84d47fb5d2f15bf02f221b3d7cb7b818189c9
colemanja91 <colemanja91@gmail.com>
2020-04-04 20:47:50 -0400

    add cognito user pool secrets

--- config/initializers/cognito.rb
+++ config/initializers/cognito.rb
@@ -1,13 +1,13 @@
 # frozen_string_literal: true
 
-# require 'cognito_jwt_keys'
-# require 'cognito_urls'
+require 'cognito_jwt_keys'
+require 'cognito_urls'
 
-# if Rails.env.production?
-#   CognitoUrls.init(
-#     Rails.application.credentials.cognito[:domain] || 'localhost',
-#     Rails.application.credentials.cognito[:region] || 'us-west-2'
-#   )
+if Rails.env.production?
+  CognitoUrls.init(
+    Rails.application.credentials.cognito[:domain] || 'localhost',
+    Rails.application.credentials.cognito[:region] || 'us-west-2'
+  )
 
-#   CognitoJwtKeysProvider.init
-# end
+  CognitoJwtKeysProvider.init
+end
