2381b94c779c650b1ae8f8d574698f0741be653b
colemanja91 <colemanja91@gmail.com>
2020-04-05 10:53:48 -0400

    fix cognito init; use node 12

--- config/initializers/cognito.rb
+++ config/initializers/cognito.rb
@@ -5,8 +5,8 @@ require 'cognito_urls'
 
 if Rails.env.production?
   CognitoUrls.init(
-    Rails.application.credentials.cognito[:domain] || 'localhost',
-    Rails.application.credentials.cognito[:region] || 'us-west-2'
+    Rails.application.credentials.cognito[:domain],
+    Rails.application.credentials.cognito[:region]
   )
 
   CognitoJwtKeysProvider.init
