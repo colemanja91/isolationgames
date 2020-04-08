b5b5aafab3ed366dce8f1109e0e858367896495e
colemanja91 <colemanja91@gmail.com>
2020-04-08 09:08:27 -0400

    better handle for setting envvars

--- script/create_production.sh
+++ script/create_production.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 eb create isolationgames-production \
-  --instance-types m5.large \
   --instance-type m5.large \
-  --min-instances 1 \
-  --max-instances 2 \
-  --elb-type application 
+  --single \
+  --envvars `paste -d, -s ./envvars`
