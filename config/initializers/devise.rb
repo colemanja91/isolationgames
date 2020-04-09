53e03060c0ef131eca74c894e1f3c304961f2bfc
colemanja91 <colemanja91@gmail.com>
2020-04-09 15:38:21 -0400

    this breaks in production

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -263,7 +263,7 @@ Devise.setup do |config|
   config.omniauth :google_oauth2,
                   Rails.application.credentials.google&.[](:client_id) || 'client_id',
                   Rails.application.credentials.google&.[](:client_secret) || 'client_secret',
-                  provider_ignores_state: Rails.env.development? || Rails.env.test?
+                  provider_ignores_state: true
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
