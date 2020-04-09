6697acbea046506c4643c99661100a40b802968e
colemanja91 <colemanja91@gmail.com>
2020-04-08 21:29:19 -0400

    remove subscriber

--- spec/factories/user_factory.rb
+++ spec/factories/user_factory.rb
@@ -1,6 +1,7 @@
+# frozen_string_literal: true
+
 FactoryBot.define do
   factory :user, class: User do
     email { Faker::Internet.unique.email }
-    subscriber { Faker::Alphanumeric.alpha(number: 10) }
   end
 end
