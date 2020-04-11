25cef5e6e41102bdd631d503ffd9ddc38a04ab52
colemanja91 <colemanja91@gmail.com>
2020-04-11 11:05:29 -0400

    fix login page styling

--- app/javascript/packs/login.js
+++ app/javascript/packs/login.js
@@ -7,6 +7,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import Login from "../components/Login";
+import "../styles/App.scss";
 
 document.addEventListener("DOMContentLoaded", () => {
   ReactDOM.render(
