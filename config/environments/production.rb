be121858846ffca112788e49591022b8db2f3b4c
colemanja91 <colemanja91@gmail.com>
2020-04-11 13:31:15 -0400

    wonder if this is it?

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,7 +4,7 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
   config.assets.compile = false
 
-  config.react.variant = :production
+  # config.react.variant = :production
 
   # Code is not reloaded between requests.
   config.cache_classes = true
