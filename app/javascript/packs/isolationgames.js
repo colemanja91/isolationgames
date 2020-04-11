d0d0861b361d738dae23458e7be9376ec35ddfa8
colemanja91 <colemanja91@gmail.com>
2020-04-11 11:11:00 -0400

    remove unused requires

--- app/javascript/packs/isolationgames.js
+++ app/javascript/packs/isolationgames.js
@@ -1,8 +1,3 @@
-require("@rails/ujs").start();
-require("turbolinks").start();
-require("@rails/activestorage").start();
-require("channels");
-
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
