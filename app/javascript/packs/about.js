4fe09d55a58613b8068cc470c6c0a6136654c355
colemanja91 <colemanja91@gmail.com>
2020-04-11 11:00:37 -0400

    fix about-public styles

--- app/javascript/packs/about.js
+++ app/javascript/packs/about.js
@@ -7,6 +7,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import About from "../components/About";
+import "../styles/App.scss";
 
 document.addEventListener("DOMContentLoaded", () => {
   ReactDOM.render(
