65891a4d24d99eaf5eec693c69d2229f3ccd6185
colemanja91 <colemanja91@gmail.com>
2020-04-22 16:06:52 -0400

    this was giving us trouble in the beta

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+  skip_before_action :verify_authenticity_token
   before_action :authenticate_user!
 
   def after_sign_in_path_for(_resource)
