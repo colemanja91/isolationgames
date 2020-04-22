d0747f4469d8a95b087dd75f2ebf4ddfa26c23f6
colemanja91 <colemanja91@gmail.com>
2020-04-22 16:19:04 -0400

    just remove the protect forgery?

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-  skip_before_action :verify_authenticity_token
   before_action :authenticate_user!
 
   def after_sign_in_path_for(_resource)
