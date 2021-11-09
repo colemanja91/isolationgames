980ca06c7b7b8ddf93e7a4113eab15ea10f5148c
Jeremiah Coleman <colemanja91@gmail.com>
2021-11-09 11:10:43 -0500

    make updates to file

--- app/graphql/isolationgames_schema.rb
+++ app/graphql/isolationgames_schema.rb
@@ -5,6 +5,8 @@ class IsolationgamesSchema < GraphQL::Schema
   # Opt in to the new runtime (default in future graphql-ruby versions)
   use GraphQL::Execution::Interpreter
   use GraphQL::Analysis::AST
+  
+  # Adding a change that doesn't matter
 
   # Add built-in connections for pagination
   use GraphQL::Pagination::Connections
